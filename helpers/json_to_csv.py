fb880e0c1eb6803bec23e935f6d5fa5510aed488
Kan Ouivirach <zkan.cs@gmail.com>
2024-01-04 16:56:01 +0000

    Move scripts to helpers

--- helpers/json_to_csv.py
+++ helpers/json_to_csv.py
@@ -7,19 +7,25 @@ with open("products.json") as f:
 
 print("### Products ###")
 print(products[0])
+print(products[99])
+print(products[999])
+print("-" * 5)
 
 with open("products.csv", "w") as csvfile:
     writer = csv.writer(csvfile)
-    header = ["sku", "name", "description"]
+    header = ["sku", "name", "description", "most_general_category"]
     writer.writerow(header)
     for each in products:
-        writer.writerow([each["sku"], each["name"], each["description"]])
+        writer.writerow([each["sku"], each["name"], each["description"], each["category"][0]["name"]])
 
 with open("categories.json") as f:
     categories = json.load(f)
 
 print("### Categories ###")
 print(categories[0])
+print(categories[99])
+print(categories[999])
+print("-" * 5)
 
 with open("categories.csv", "w") as csvfile:
     writer = csv.writer(csvfile)
@@ -33,6 +39,9 @@ with open("stores.json") as f:
 
 print("### Stores ###")
 print(stores[0])
+print(stores[99])
+print(stores[999])
+print("-" * 5)
 
 with open("stores.csv", "w") as csvfile:
     writer = csv.writer(csvfile)
